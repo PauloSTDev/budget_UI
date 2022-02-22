e304daad7920e62ea65ccd19e06292f60558d9ea
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-21 22:21:05 -0300

    Implementa SliverAppBar com botão settings, add e o title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -9,6 +8,33 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            forceElevated: true,
+            floating: true,
+            expandedHeight: 100.0,
+            leading: IconButton(
+              icon: Icon(Icons.settings),
+              iconSize: 30.0,
+              onPressed: () {},
+            ),
+            backgroundColor: Colors.green,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("Simple Budget"),
+              centerTitle: true,
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.add),
+                iconSize: 30.0,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
