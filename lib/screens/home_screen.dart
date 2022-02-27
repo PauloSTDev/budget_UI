4c71d660ea283d8d46a17380a3e829670e9e1650
Paulo Santos <afonso.mea@gmail.com>
2022-02-27 13:46:44 -0300

    Cria category_screen.dart e adiciona navegação de pages

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_budget_ui/data/data.dart';
 import 'package:flutter_budget_ui/helpers/color_helper.dart';
 import 'package:flutter_budget_ui/models/category_model.dart';
 import 'package:flutter_budget_ui/models/expense_model.dart';
+import 'package:flutter_budget_ui/screens/category_screen.dart';
 import 'package:flutter_budget_ui/widgets/bar_chart.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -13,72 +14,76 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   //Build para apresentar as categorias e os gastos
   _buildCategory(Category, double totalAmountSpent) {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-      height: 100.0,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 2),
-            blurRadius: 6.0,
-          ),
-        ],
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                Category.name,
-                style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
-              ),
-              Text(
-                "\$${(Category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${(Category.maxAmount).toStringAsFixed(2)}",
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0),
-              ),
-            ],
-          ),
-          SizedBox(height: 10.0),
-          LayoutBuilder(
-            builder: (BuildContext context, BoxConstraints constraints){
-              final double maxBarWidth = constraints.maxWidth;
-              final double percent = (Category.maxAmount - totalAmountSpent) / Category.maxAmount;
-              double barWidth = percent * maxBarWidth;
+    return GestureDetector(
+      onTap: () => Navigator.push(context, MaterialPageRoute(builder: (_) => CategoryScreen(category: Category),
+          )),
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        height: 100.0,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black12,
+              offset: Offset(0, 2),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  Category.name,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                ),
+                Text(
+                  "\$${(Category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${(Category.maxAmount).toStringAsFixed(2)}",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0),
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints){
+                final double maxBarWidth = constraints.maxWidth;
+                final double percent = (Category.maxAmount - totalAmountSpent) / Category.maxAmount;
+                double barWidth = percent * maxBarWidth;
 
-              if (barWidth < 0){
-                barWidth = 0;
-              }
+                if (barWidth < 0){
+                  barWidth = 0;
+                }
 
 
-              return Stack(
-                children: [
-                  Container(
-                    height: 20.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[200],
-                      borderRadius: BorderRadius.circular(15.0),
+                return Stack(
+                  children: [
+                    Container(
+                      height: 20.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 20.0,
-                    width: barWidth,
-                    decoration: BoxDecoration(
-                      color: getColor(context, percent),
-                      borderRadius: BorderRadius.circular(15.0),
+                    Container(
+                      height: 20.0,
+                      width: barWidth,
+                      decoration: BoxDecoration(
+                        color: getColor(context, percent),
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
                     ),
-                  ),
-                ],
-              );
-            },
-          ),
-        ],
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
