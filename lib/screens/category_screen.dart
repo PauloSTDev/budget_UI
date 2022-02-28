34982fff71b129463eebd3a27807cd9aaa96c688
Paulo Santos - AMD <afonso.mea@gmail.com>
2022-02-28 19:54:32 -0300

    Adiciona SingleChildScrollView na category_screen.dart

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -14,7 +14,49 @@ class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar:AppBar(
+        title: Text(widget.category.name),
+        backgroundColor: Theme.of(context).primaryColor,
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: Icon(Icons.add),
+            iconSize: 30.0,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(20.0),
+              padding: EdgeInsets.all(20.0),
+              height: 250.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10.0),
+                //boxShadow cria uma sombra envolta do container em que está
+                boxShadow: [
+                  BoxShadow(
+                    //Cor co BoxShadow
+                    color: Colors.black12,
+                    //Define uma direção em que fica mais aparente a sombra
+                    //seja na direita ou esquerda, baixo ou cima, pode ser usado
+                    //valores negativos pois se trata de valor x e y
+                    offset: Offset(0, 2),
+                    //Dispersão da sombra, aumenta/diminue a sombra
+                    //Só aceita valores >= 0
+                    blurRadius: 6.0,
+                  ),
+                ],
+              ),
 
+            )
+          ],
+        ),
+      ),
     );
   }
 }
